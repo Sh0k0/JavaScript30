0b7415ee91deef60ea48967ea2a19392be879d1b
Sh0k0 <vlad6661866@gmail.com>
2021-10-28 19:33:44 +0300

    feat: add all keys

--- piano/script.js
+++ piano/script.js
@@ -1,16 +1,34 @@
 let keys = document.querySelectorAll('.key');
 
 keys.forEach(key => {
- key.addEventListener('click', playNote);
+  key.addEventListener('click', playNote);
 });
 
 function playNote(e) {
- let key = e.target;
- let note = document.getElementById(key.dataset.note);
- key.classList.add('active');
- note.currentTime = 0;
- note.play();
- note.addEventListener('ended',() => {
-  key.classList.remove('active');
- });
+  let key = e.target;
+  let note = document.getElementById(key.dataset.note);
+  key.classList.add('active');
+  note.currentTime = 0;
+  note.play();
+  note.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
 }
+
+
+window.addEventListener('keydown', function (e) {
+  if (e.shiftKey) {
+    audio = document.querySelector(`audio[data-key="shift+${e.keyCode}"]`);
+    key = document.querySelector(`.key[data-key="shift+${e.keyCode}"]`);
+  } else {
+    audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  }
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('active');
+  audio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+});
